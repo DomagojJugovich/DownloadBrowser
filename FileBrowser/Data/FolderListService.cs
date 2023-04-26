ae9e315ace1d5a9ff640911833c24bfb2972f372
Domagoj Jugovic <domagoj.jugovic@gmail.com>
2023-04-26 10:29:25 +0200

    inital version for internal usage

---
+++ FileBrowser/Data/FolderListService.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting.Server;
+
+namespace FileBrowser.Data
+{
+    public class FolderListService
+    {
+      
+
+        public IEnumerable<String> GetFiles()
+        {
+
+            string rootpath = AppDomain.CurrentDomain.BaseDirectory;
+
+            rootpath =  System.IO.Path.Combine(rootpath, "wwwroot");
+            rootpath =  System.IO.Path.Combine(rootpath, "packages");
+
+            return Directory. EnumerateFiles(rootpath);
+        
+        }
+    }
+}
